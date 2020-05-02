e5e8c7ebcf69f83263b2051f5f09a8207c89128e
huanshenyi <txy1226052@gmail.com>
2020-05-02 22:50:50 +0900

    カメラ撮影アプリの完成, Wrap,GestureDetector,ClipRRect,Positionedの使用

--- lib/photo_app_page.dart
+++ lib/photo_app_page.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+import 'package:image_picker/image_picker.dart';
 
 // カメラアプリ開発
 class PhotoApp extends StatefulWidget {
@@ -8,11 +10,21 @@ class PhotoApp extends StatefulWidget {
 }
 
 class _PhotoAppState extends State<PhotoApp> {
+  List<File> _images = [];
+  Future getImage(bool isTakePhoto) async {
+    // ボタンリストを閉じる
+    Navigator.pop(context);
+    var image = await ImagePicker.pickImage(source: isTakePhoto? ImageSource.camera: ImageSource.gallery);
+    setState(() {
+      _images.add(image);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("リソース使用"),
+          title: Text("写真app"),
           leading: GestureDetector( //左側のwidget
             onTap: (){
               Navigator.pop(context); //現在のページから脱出
@@ -21,17 +33,73 @@ class _PhotoAppState extends State<PhotoApp> {
           )
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-                width: 100,
-                height: 100,
-                image: AssetImage("images/vuex.png")
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Wrap(
+          spacing: 5,
+          runSpacing: 5,
+          children: _getImages(),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImage,
+        tooltip: '画像を選択',
+        child: Icon(Icons.add_a_photo),
+      ),
     );
+  }
+  _pickImage() async {
+    //下にある上がってくるエリア
+    showModalBottomSheet(context: context, builder: (context)=>Container(
+      height: 160,
+      child: Column(
+        children: <Widget>[
+          _item("写真撮影", true),
+          _item("アルバムから選択", false),
+        ],
+      ),
+    ));
+  }
+  _item(String title, bool isTakePhoto){
+    //クリックできるボタン
+     return GestureDetector(
+        child: ListTile(
+          leading: Icon(isTakePhoto ? Icons.camera_alt: Icons.photo_library),
+          title: Text(title),
+          onTap: () => getImage(isTakePhoto)
+        ),
+     );
+  }
+  _getImages(){
+   return _images.map((file){
+      return Stack(
+        children: <Widget>[
+          //正方形の容器
+          ClipRRect(
+            //容器の円角
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(file, width: 120, height: 90,fit: BoxFit.fill),
+          ),
+          //右上にある削除ボタン
+          Positioned(
+            right: 5,
+            top: 5,
+            child: GestureDetector(
+              onTap: (){
+                setState(() {
+                  _images.remove(file);
+                });
+              },
+              //円角の削除ボタン
+              child: ClipOval(
+                child: Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(color: Colors.black45),
+                  child: Icon(Icons.close, size: 18,color: Colors.white,),
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    }).toList();
   }
 }
